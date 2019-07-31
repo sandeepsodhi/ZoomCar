8077bfe617d2d5c93e5b2d90d5d7d28792fe956f
sandeepsodhi <vickysodhi15@gmail.com>
2019-07-31 14:16:58 -0400

    Modifying main activity file

--- ZoomCar/MainActivity.cs
+++ ZoomCar/MainActivity.cs
@@ -12,6 +12,7 @@ namespace ZoomCar
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
