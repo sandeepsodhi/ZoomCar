c781029f3ee46f31518d740c09936e88d1f5bd90
sandeepsodhi <vickysodhi15@gmail.com>
2019-08-01 11:25:05 -0400

    Login and signUp sceen

--- ZoomCar/MainActivity.cs
+++ ZoomCar/MainActivity.cs
@@ -3,18 +3,40 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace ZoomCar
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button loginBtn, signUpBtn;
+        Intent i;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            loginBtn = FindViewById<Button>(Resource.Id.button1);
+            signUpBtn = FindViewById<Button>(Resource.Id.button2);
+
+            loginBtn.Click += LoginBtn_Click;
+            signUpBtn.Click += SignUpBtn_Click;
+
+        }
+
+        private void SignUpBtn_Click(object sender, System.EventArgs e)
+        {
+            i = new Intent(this, typeof(SignUp));
+            StartActivity(i);
+        }
+
+        private void LoginBtn_Click(object sender, System.EventArgs e)
+        {
+            i = new Intent(this, typeof(Login));
+            StartActivity(i);
         }
     }
 }
