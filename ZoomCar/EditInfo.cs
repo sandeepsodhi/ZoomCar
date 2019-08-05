4dc808a15b55f77056843a0771c5c98b01cb57d7
Rajeshsaini123 <53200467+Rajeshsaini123@users.noreply.github.com>
2019-08-05 16:41:13 -0400

    changes for updating user profi

--- ZoomCar/EditInfo.cs
+++ ZoomCar/EditInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,22 @@ namespace ZoomCar
 {
     public class EditInfo : Fragment
     {
+        DBHelper myDbInstace;
+        TextView welcomeMessageText;
+        EditText fnameEdit;
+        EditText lnameEdit;
+        EditText emailEdit;
+        EditText ageEdit;
+        EditText passwordEdit;
+        Button updateBtn, deleteBtn;
+
+        string id;
+
+        public EditInfo(string idIntent)
+        {
+            this.id = idIntent;
+        }
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -27,7 +43,31 @@ namespace ZoomCar
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
             View myView = inflater.Inflate(Resource.Layout.EditInfo, container, false);
+            
+            welcomeMessageText = myView.FindViewById<TextView>(Resource.Id.welcomMessage);
+            fnameEdit = myView.FindViewById<EditText>(Resource.Id.fNameEdit);
+            lnameEdit = myView.FindViewById<EditText>(Resource.Id.lNameEdit);
+            emailEdit = myView.FindViewById<EditText>(Resource.Id.emailEdit);
+            ageEdit = myView.FindViewById<EditText>(Resource.Id.ageEdit);
+            passwordEdit = myView.FindViewById<EditText>(Resource.Id.passwordEdit);
 
+            EnabledisableEditBox(false);
+
+            myDbInstace = new DBHelper(Activity);
+            user userInfo = myDbInstace.selectMyValues(id);
+
+            welcomeMessageText.Text = "Welcome " + userInfo.fname;
+            fnameEdit.Text = userInfo.fname;
+            lnameEdit.Text = userInfo.lname;
+            emailEdit.Text = userInfo.email;
+            ageEdit.Text = userInfo.age;
+            passwordEdit.Text = userInfo.password;
+
+            updateBtn = myView.FindViewById<Button>(Resource.Id.btnUpdate);
+            updateBtn.Click += updateInfo;
+
+            deleteBtn = myView.FindViewById<Button>(Resource.Id.btnDelete);
+            deleteBtn.Click += deleteRecord;
             return myView;
 
 //            return base.OnCreateView(inflater, container, savedInstanceState);
@@ -37,6 +77,45 @@ namespace ZoomCar
             base.OnResume();
             System.Console.WriteLine("OnResume");
 
+        }
+
+        private void updateInfo(object sender, EventArgs e)
+        {
+            if (!fnameEdit.Enabled)
+            {
+                EnabledisableEditBox(true);
+                updateBtn.Text = "Update";
+            }
+            else
+            {
+                var vfname = fnameEdit.Text;
+                var vlname = lnameEdit.Text;
+                var vage = ageEdit.Text;
+                var vpassword = passwordEdit.Text;
+                var vemail = emailEdit.Text;
+                myDbInstace.updateData(id, vfname, vlname, vage, vpassword, vemail);
+                updateBtn.Text = "Edit";
+                EnabledisableEditBox(false);
+                Toast.MakeText(Activity, "Update successfull", ToastLength.Long).Show();
+            }
+        }
+
+        private void deleteRecord(object sender, EventArgs e)
+        {
+            myDbInstace.deleteData(id);
+            Toast.MakeText(Activity, "Deletion successful", ToastLength.Long).Show();
+
+            Intent i = new Intent(Activity, typeof(Login));
+            StartActivity(i);
+        }
+
+        private void EnabledisableEditBox(bool flag)
+        {
+            fnameEdit.Enabled = flag;
+            lnameEdit.Enabled = flag;
+            emailEdit.Enabled = flag;
+            ageEdit.Enabled = flag;
+            passwordEdit.Enabled = flag;
         }
     }
 }
