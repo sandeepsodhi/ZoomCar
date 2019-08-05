6e936df565fe1e3639c0c241920cbcd9efa63be0
Rajeshsaini123 <53200467+Rajeshsaini123@users.noreply.github.com>
2019-08-05 16:48:41 -0400

    resouce file update

--- ZoomCar/Resources/Resource.designer.cs
+++ ZoomCar/Resources/Resource.designer.cs
@@ -2351,8 +2351,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int action0 = 2131230896;
+			// aapt resource value: 0x7f0800af
+			public const int action0 = 2131230895;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2375,17 +2375,17 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int action_container = 2131230893;
+			// aapt resource value: 0x7f0800ac
+			public const int action_container = 2131230892;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800b4
-			public const int action_divider = 2131230900;
+			// aapt resource value: 0x7f0800b3
+			public const int action_divider = 2131230899;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int action_image = 2131230894;
+			// aapt resource value: 0x7f0800ad
+			public const int action_image = 2131230893;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2402,11 +2402,11 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800af
-			public const int action_text = 2131230895;
+			// aapt resource value: 0x7f0800ae
+			public const int action_text = 2131230894;
 			
-			// aapt resource value: 0x7f0800bd
-			public const int actions = 2131230909;
+			// aapt resource value: 0x7f0800bc
+			public const int actions = 2131230908;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2414,8 +2414,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080027
 			public const int add = 2131230759;
 			
-			// aapt resource value: 0x7f0800c4
-			public const int age = 2131230916;
+			// aapt resource value: 0x7f0800c3
+			public const int age = 2131230915;
 			
 			// aapt resource value: 0x7f0800a2
 			public const int ageEdit = 2131230882;
@@ -2444,14 +2444,11 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08003d
 			public const int bottom = 2131230781;
 			
-			// aapt resource value: 0x7f0800a5
-			public const int btnAllUsers = 2131230885;
-			
 			// aapt resource value: 0x7f0800a4
 			public const int btnDelete = 2131230884;
 			
-			// aapt resource value: 0x7f0800c5
-			public const int btnRegister = 2131230917;
+			// aapt resource value: 0x7f0800c4
+			public const int btnRegister = 2131230916;
 			
 			// aapt resource value: 0x7f0800a3
 			public const int btnUpdate = 2131230883;
@@ -2465,8 +2462,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int cancel_action = 2131230897;
+			// aapt resource value: 0x7f0800b0
+			public const int cancel_action = 2131230896;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2480,8 +2477,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800b9
-			public const int chronometer = 2131230905;
+			// aapt resource value: 0x7f0800b8
+			public const int chronometer = 2131230904;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2534,8 +2531,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08007e
 			public const int edit_query = 2131230846;
 			
-			// aapt resource value: 0x7f0800c2
-			public const int email = 2131230914;
+			// aapt resource value: 0x7f0800c1
+			public const int email = 2131230913;
 			
 			// aapt resource value: 0x7f0800a0
 			public const int emailEdit = 2131230880;
@@ -2543,8 +2540,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800bf
-			public const int end_padder = 2131230911;
+			// aapt resource value: 0x7f0800be
+			public const int end_padder = 2131230910;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2561,8 +2558,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080072
 			public const int expanded_menu = 2131230834;
 			
-			// aapt resource value: 0x7f0800c0
-			public const int fName = 2131230912;
+			// aapt resource value: 0x7f0800bf
+			public const int fName = 2131230911;
 			
 			// aapt resource value: 0x7f08009e
 			public const int fNameEdit = 2131230878;
@@ -2582,11 +2579,11 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080058
 			public const int forever = 2131230808;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int forgotPassword = 2131230891;
+			// aapt resource value: 0x7f0800aa
+			public const int forgotPassword = 2131230890;
 			
-			// aapt resource value: 0x7f0800c6
-			public const int frameLayout1 = 2131230918;
+			// aapt resource value: 0x7f0800c5
+			public const int frameLayout1 = 2131230917;
 			
 			// aapt resource value: 0x7f08000a
 			public const int ghost_view = 2131230730;
@@ -2600,8 +2597,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800be
-			public const int icon_group = 2131230910;
+			// aapt resource value: 0x7f0800bd
+			public const int icon_group = 2131230909;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2612,8 +2609,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08008b
 			public const int imageView1 = 2131230859;
 			
-			// aapt resource value: 0x7f0800ba
-			public const int info = 2131230906;
+			// aapt resource value: 0x7f0800b9
+			public const int info = 2131230905;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2621,8 +2618,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
 			
-			// aapt resource value: 0x7f0800c1
-			public const int lName = 2131230913;
+			// aapt resource value: 0x7f0800c0
+			public const int lName = 2131230912;
 			
 			// aapt resource value: 0x7f08009f
 			public const int lNameEdit = 2131230879;
@@ -2645,23 +2642,23 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08001d
 			public const int listMode = 2131230749;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int listView1 = 2131230887;
+			// aapt resource value: 0x7f0800a6
+			public const int listView1 = 2131230886;
 			
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int login = 2131230890;
+			// aapt resource value: 0x7f0800a9
+			public const int login = 2131230889;
 			
-			// aapt resource value: 0x7f0800c9
-			public const int masked = 2131230921;
+			// aapt resource value: 0x7f0800c8
+			public const int masked = 2131230920;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int media_actions = 2131230899;
+			// aapt resource value: 0x7f0800b2
+			public const int media_actions = 2131230898;
 			
-			// aapt resource value: 0x7f0800c7
-			public const int message = 2131230919;
+			// aapt resource value: 0x7f0800c6
+			public const int message = 2131230918;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2684,14 +2681,14 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800bc
-			public const int notification_background = 2131230908;
-			
-			// aapt resource value: 0x7f0800b6
-			public const int notification_main_column = 2131230902;
+			// aapt resource value: 0x7f0800bb
+			public const int notification_background = 2131230907;
 			
 			// aapt resource value: 0x7f0800b5
-			public const int notification_main_column_container = 2131230901;
+			public const int notification_main_column = 2131230901;
+			
+			// aapt resource value: 0x7f0800b4
+			public const int notification_main_column_container = 2131230900;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2702,14 +2699,14 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08000b
 			public const int parent_matrix = 2131230731;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int password = 2131230889;
+			// aapt resource value: 0x7f0800a8
+			public const int password = 2131230888;
 			
 			// aapt resource value: 0x7f0800a1
 			public const int passwordEdit = 2131230881;
 			
-			// aapt resource value: 0x7f0800c3
-			public const int phoneNumber = 2131230915;
+			// aapt resource value: 0x7f0800c2
+			public const int phoneNumber = 2131230914;
 			
 			// aapt resource value: 0x7f08004d
 			public const int pin = 2131230797;
@@ -2726,11 +2723,11 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800bb
-			public const int right_icon = 2131230907;
+			// aapt resource value: 0x7f0800ba
+			public const int right_icon = 2131230906;
 			
-			// aapt resource value: 0x7f0800b7
-			public const int right_side = 2131230903;
+			// aapt resource value: 0x7f0800b6
+			public const int right_side = 2131230902;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2759,8 +2756,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080055
 			public const int scrollable = 2131230805;
 			
-			// aapt resource value: 0x7f0800a6
-			public const int searchView1 = 2131230886;
+			// aapt resource value: 0x7f0800a5
+			public const int searchView1 = 2131230885;
 			
 			// aapt resource value: 0x7f080080
 			public const int search_badge = 2131230848;
@@ -2807,8 +2804,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080025
 			public const int showTitle = 2131230757;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int signUp = 2131230892;
+			// aapt resource value: 0x7f0800ab
+			public const int signUp = 2131230891;
 			
 			// aapt resource value: 0x7f08008f
 			public const int smallLabel = 2131230863;
@@ -2840,8 +2837,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int status_bar_latest_event_content = 2131230898;
+			// aapt resource value: 0x7f0800b1
+			public const int status_bar_latest_event_content = 2131230897;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2876,8 +2873,8 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800b8
-			public const int time = 2131230904;
+			// aapt resource value: 0x7f0800b7
+			public const int time = 2131230903;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2921,14 +2918,14 @@ namespace ZoomCar
 			// aapt resource value: 0x7f080026
 			public const int useLogo = 2131230758;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int userName = 2131230888;
+			// aapt resource value: 0x7f0800a7
+			public const int userName = 2131230887;
 			
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800c8
-			public const int visible = 2131230920;
+			// aapt resource value: 0x7f0800c7
+			public const int visible = 2131230919;
 			
 			// aapt resource value: 0x7f08009d
 			public const int welcomMessage = 2131230877;
